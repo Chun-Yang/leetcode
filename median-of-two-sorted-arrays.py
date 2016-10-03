6fdc3ede57acd85928e5a3d91e01b44437b7efd5
Chun Yang <yang2007chun@gmail.com>
2016-10-02 19:11:19 -0500

    finish

--- median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays.py
@@ -9,20 +9,23 @@ class Solution(object):
         else:
             return nums[(nums_len) // 2]
 
+    def getInner(self, nums, length):
+        if length <= 3:
+            return nums
+        elif length % 2 == 0:
+            return nums[length // 2 - 1 : length // 2 + 1]
+        else:
+            return nums[length // 2 - 1 : length // 2 + 2]
+
+    def isCrossing(self, nums1, nums2):
+        not_crossing = nums1[-1] < nums2[0] or nums1[0] > nums2[-1]
+        return not not_crossing
+
     def shorten(self, nums, length, remove_larger):
         if remove_larger:
             return nums[:-length]
         else:
             return nums[length:]
-
-    def isInBetweenThree(self, one, three):
-        middle = len(three) // 2
-        return  one <= three[middle] and one >= three[middle - 1] or \
-                one >= three[middle] and one <= three[middle + 1]
-
-    def isInBetweenTwo(self, one, two):
-        middle = len(two) // 2
-        return one >= two[middle - 1] and one <= two[middle]
 
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -37,42 +40,25 @@ class Solution(object):
             return self.findMedian(nums2)
         elif nums2_len == 0:
             return self.findMedian(nums1)
-        elif nums1_len == 1 and nums2_len == 1:
-            return (nums1[0] + nums2[0]) / 2.0
+        elif nums1_len <= 3 and nums2_len <= 3:
+            return self.findMedian(sorted(nums1 + nums2))
         else:
-            nums1_median = self.findMedian(nums1)
-            nums2_median = self.findMedian(nums2)
-            if nums1_median == nums2_median:
-                return nums1_median
+            nums1_inner = self.getInner(nums1, nums1_len)
+            nums2_inner = self.getInner(nums2, nums2_len)
+            if self.isCrossing(nums1_inner, nums2_inner):
+                return self.findMedianSortedArrays(nums1_inner, nums2_inner)
             else:
-                is_single = (nums1_len + nums2_len) % 2 == 1
-
-                one_and_three = (not is_single) and ( \
-                    (nums1_len == 1 and self.isInBetweenThree(nums1[0], nums2)) or \
-                    (nums2_len == 1 and self.isInBetweenThree(nums2[0], nums1)) \
-                    )
-
-                if one_and_three:
-                    return (nums1_median + nums2_median) / 2.0
-
-                one_and_two = is_single and ( \
-                    (nums1_len == 1 and self.isInBetweenTwo(nums1[0], nums2)) or \
-                    (nums2_len == 1 and self.isInBetweenTwo(nums2[0], nums1)) \
-                    )
-
-                if one_and_two:
-                    return nums1[0] if nums1_len == 1 else nums2[0]
-
                 min_len = max(1, min(nums1_len, nums2_len) // 2)
-                nums1_bigger = nums1_median > nums2_median
+                nums1_bigger = nums1_inner[0] > nums2_inner[0]
                 return self.findMedianSortedArrays(
                         self.shorten(nums1, min_len, nums1_bigger),
                         self.shorten(nums2, min_len, not nums1_bigger)
                         )
+
 
 # assert Solution().findMedianSortedArrays([1, 3], [2]) == 2
 # assert Solution().findMedianSortedArrays([1, 2], [3, 4]) == 2.5
 # assert Solution().findMedianSortedArrays([2], [1, 3, 4]) == 2.5
 # assert Solution().findMedianSortedArrays([2, 3], [1, 4, 5]) == 3
 # assert Solution().findMedianSortedArrays([3], [1, 2, 4, 5, 6]) == 3.5
-assert Solution().findMedianSortedArrays([1, 2, 6], [3, 4, 5]) == 3.5
+# assert Solution().findMedianSortedArrays([1, 2, 6], [3, 4, 5]) == 3.5

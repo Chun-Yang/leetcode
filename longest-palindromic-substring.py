59faf8ccaf2d15684b82f6e228a12ab7dd68a7cd
Chun Yang <yang2007chun@gmail.com>
2016-10-04 08:30:58 -0500

    n^2

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -1,26 +1,52 @@
 class Solution(object):
-    def __init__(self):
-        self.memoization = {}
+    def longestWithCenter(self, padded_s, i):
+        length = 0
+        while padded_s[i - length - 1] == padded_s[i + length + 1]:
+            length += 1
 
-    def isPalindrome(self, s):
-        if s in self.memoization:
-            return self.memoization[s]
+        # print("i", i)
+        # print("length", length)
+        # string = padded_s[
+        #         i - length:
+        #         i + length + 1
+        #         ]
+        # print("string", string)
 
-        is_palindrome = len(s) <= 1 or (s[0] == s[-1] and self.isPalindrome(s[1: -1]))
-        self.memoization[s] = is_palindrome
-        return is_palindrome
+        return length
 
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        if self.isPalindrome(s):
+        if len(s) == 0:
             return s
-        else:
-            first_half = self.longestPalindrome(s[1:])
-            second_half = self.longestPalindrome(s[:-1])
-            return first_half if (len(first_half) > len(second_half)) else second_half
+
+        padded_s = '^#' + '#'.join(list(s)) + '#$'
+        # NOTE: the length is real, the string is padded
+
+        padded_len = len(padded_s)
+        longest_center = 1
+        longest_length = 0
+
+        for i in range(1, len(padded_s) - 1):
+            length = self.longestWithCenter(padded_s, i)
+            if length > longest_length:
+                longest_length = length
+                longest_center = i
+
+        longest_string = ''.join(filter(
+            lambda x: x != '#',
+            list(padded_s[
+                longest_center - longest_length:
+                longest_center + longest_length + 1
+            ])
+        ))
+
+        # print("longest_string", longest_string)
+
+        return longest_string
 
 
-assert Solution().longestPalindrome('abba') == 'abba'
+# assert Solution().longestPalindrome('abba') == 'abba'
+# assert Solution().longestPalindrome('ccd') == 'cc'

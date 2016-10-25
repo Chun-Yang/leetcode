5c7bf7d00c68f22ce514c87d4647723eaf956e85
Chun Yang <yang2007chun@gmail.com>
2016-10-24 19:59:05 -0500

    chang

--- word-break-ii.py
+++ word-break-ii.py
@@ -5,24 +5,35 @@ class Solution(object):
         :type wordDict: Set[str]
         :rtype: List[str]
         """
-        memo = [None] * (len(s) + 1)
-        memo[len(s)] = ['']
+        memo = {len(s): ['']}
         def sentences(i):
-            if memo[i] != None:
+            if i in memo:
                 return memo[i]
 
             res = []
             for j in range(i + 1, len(s) + 1):
                 if s[i:j] in wordDict:
                     for sentence in sentences(j):
-                        res.append(s[i:j] + ' ' + sentence)
+                        res.append(s[i:j] + (sentence and ' ' + sentence))
 
             memo[i] = res
             return res
 
-        res = [sentence[:-1] for sentence in sentences(0)]
-        return res
+        return sentences(0)
 
+# class Solution(object):
+#     def wordBreak(self, s, wordDict):
+#         memo = {len(s): ['']}
+#         def sentences(i):
+#             if i not in memo:
+#                 strings = []
+#                 for j in range(i+1, len(s)+1):
+#                     if s[i:j] in wordDict:
+#                         for tail in sentences(j):
+#                             strings.append(s[i:j] + (tail and ' ' + tail))
+#                 memo[i] = strings
+#             return memo[i]
+#         return sentences(0)
 
 # assert set(Solution().wordBreak("a", ['a'])) == set(['a'])
 

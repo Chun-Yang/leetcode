f62d8fd3850361d29681394eb8fb9799d0f81259
Chun Yang <yang2007chun@gmail.com>
2016-12-24 10:52:35 -0600

    more

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -1,23 +1,65 @@
+# from operator import add, sub, mul, floordiv
+
+# class Solution(object):
+#     def calculate(self, s):
+#         lowAns, lowOp, highAns, highOp, val = 0, add, None, None, 0
+#         cToOp = { '+': add, '-': sub, '*': mul, '/': floordiv }
+
+#         for c in s + '+':
+#             if c == ' ':
+#                 continue
+#             elif c == '+' or c == '-':
+#                 if highOp:
+#                     highAns = highOp(highAns, val)
+#                     lowAns = lowOp(lowAns, highAns)
+#                 else:
+#                     lowAns = lowOp(lowAns, val)
+#                 lowOp, highAns, highOp, val = cToOp[c], None, None, 0
+#             elif c == '*' or c == '/':
+#                 if highOp:
+#                     highAns = highOp(highAns, val)
+#                 else:
+#                     highAns = val
+#                 highOp, val = cToOp[c], 0
+#             else:
+#                 val = val * 10 + int(c)
+
+#         return lowAns
+
+from operator import add, sub, mul, floordiv
+
 class Solution(object):
     def calculate(self, s):
-        lowAns, lowSign, highAns, highSign, val = 0, '+', None, None, 0
-        for c in s + '+':
-            if c == ' ':
-                continue
-            elif c == '+' or c == '-':
-                if highSign:
-                    highAns = highAns * val if highSign == '*' else highAns // val
-                    lowAns = lowAns + highAns if lowSign == '+' else lowAns - highAns
-                else:
-                    lowAns = lowAns + val if lowSign == '+' else lowAns - val
-                lowSign, highAns, highSign, val = c, None, None, 0
-            elif c == '*' or c == '/':
-                if highSign:
-                    highAns = highAns * val if highSign == '*' else highAns // val
-                else:
-                    highAns = val
-                highSign, val = c, 0
-            else:
-                val = val * 10 + int(c)
+        operands, operators = [], []
+        i, size = 0, len(s)
 
-        return lowAns
+        cToOp = { '+': add, '-': sub, '*': mul, '/': floordiv }
+        def applyOp(op, right, left):
+            return cToOp[op](left, right)
+
+        while i < size:
+            c = s[i]
+            if c == ' ':
+                i += 1
+                continue
+            elif c in ['+', '-', '*', '/']:
+                i += 1
+                if c in ['+', '-'] and operators:
+                    operands.append(applyOp(operators.pop(), operands.pop(), operands.pop()))
+                operators.append(c)
+            else:
+                j = i
+                while j < size and s[j] in '0123456789':
+                    j += 1
+                value, i = int(s[i:j]), j
+                if operators and operators[-1] in ['*', '/']:
+                    operands.append(applyOp(operators.pop(), value, operands.pop()))
+                else:
+                    operands.append(value)
+
+        while operators:
+            operands.append(applyOp(operators.pop(), operands.pop(), operands.pop()))
+
+        return operands[0]
+
+# assert Solution().calculate('1 - 1 + 1') == 1

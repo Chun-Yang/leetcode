fcb06492e05be1efabef41ea3b55fe3a8b7ee9b7
Chun Yang <yang2007chun@gmail.com>
2016-12-26 21:23:57 -0600

    more

--- expression-add-operators.py
+++ expression-add-operators.py
@@ -9,47 +9,44 @@ class Solution(object):
         if not num:
             return []
 
-        def allPossible(lo, hi):
-            return []
+        # @param array of string
+        # @param target integer
+        # @return ans array of string
+        def dfs(num, target):
+            size = len(num)
+            if size == 1:
+                return num if num[0] == str(target) else []
 
-        def addOps(lo, hi, target):
-            print("num[lo:hi], target", num[lo:hi], target)
-            res = []
+            ans = []
+            # +
+            for i in dfs(num[1:], target - int(num[0])):
+                ans.append(num[0] + '+' + i)
+            # -
+            for i in dfs(['-' + num[1]] + num[2:], target - int(num[0])):
+                ans.append(num[0] + i)
+            # composition
+            if int(num[0]) != 0:
+                ans.extend(dfs([''.join(num[:2])] + num[2:], target))
+            # multiplication
+            # this couples with composition
+            if num[1] == '0':
+                for i in dfs(['0'] + num[2:], target):
+                    ans.append(num[0] + '*0' + i[1:])
+            else:
+                for i in range(1, size):
+                    left, right = num[0], ''.join(num[1:i+1])
+                    exp = left + '*' + right
+                    val = int(left) * int(right)
+                    for j in dfs([str(val)] + num[i+1:], target):
+                        ans.append(exp + j[len(str(val)):])
 
-            if hi - lo == 1:
-                s = num[lo]
-                n = int(s)
-                if n == target:
-                    res.append(s)
-                return res
+            return ans
 
-            if num[lo] == '0':
-                s = '0'
-                n = 0
-                for r in addOps(lo + 1, hi, target):
-                    res.append(s + '+' + r)
-                for r in addOps(lo + 1, hi, -target):
-                    res.append(s + '-' + r)
-                if target == n:
-                    for r in allPossible(lo + 1, hi):
-                        res.append(s + '*' + r)
+        ans = dfs(list(num), target)
+        return ans
 
-            for i in range(lo + 1, hi):
-                s = num[lo:i]
-                n = int(s)
-                for r in addOps(i, hi, target - n):
-                    res.append(s + '+' + r)
-                for r in addOps(i, hi, n - target):
-                    res.append(s + '-' + r)
-                if n != 0 and target % n == 0:
-                    for r in addOps(lo + 1, hi, target // n):
-                        res.append(s + '*' + r)
-
-            return res
-
-        ops = addOps(0, len(num), target)
-        return [exp  for exp in ops if exp != str(target)]
-
-
-# assert Solution().addOperators("123", 6) == ["1+2+3", "1*2*3"]
-assert Solution().addOperators("232", 8) == ["2*3+2","2+3*2"]
+# assert Solution().addOperators("123", 6) == ["1+2+3","1*2*3"]
+# assert Solution().addOperators("232", 8) == ["2+3*2","2*3+2"]
+# assert Solution().addOperators("121315", 160) == ['12*13-1+5']
+assert Solution().addOperators("3456237490", 9191) == []
+# ["3+456*23+74-9+0","3+456*23+74-9-0","34*56-2+3-7490"]

ff9a921456a35d4a582c6132139c05c861f0f25b
Chun Yang <yang2007chun@gmail.com>
2016-12-30 22:07:36 -0600

    more

--- count-complete-tree-nodes.py
+++ count-complete-tree-nodes.py
@@ -11,6 +11,32 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        if not root:
+            return 0
+
         h, node = 0, root
         while node:
             h, node = h + 1, node.left
+
+        # binary search to get the count of last layer
+        lastFull = 2 ** (h - 1)
+        lo, hi = 1, lastFull + 1
+        while lo < hi:
+            mid = (lo + hi) // 2
+            midTemp = mid
+            node = root
+            half = lastFull // 2
+            for i in range(h - 1):
+                if midTemp > half:
+                    midTemp -= half
+                    node = node.right
+                else:
+                    node = node.left
+                half = half // 2
+
+            if node:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return 2 ** (h - 1) - 1 + lo - 1

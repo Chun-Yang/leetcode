fd22e073fc3b70d4344071553551f3832ab1e5ce
Chun Yang <yang2007chun@gmail.com>
2017-02-10 07:55:27 -0600

    more

--- pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow.py
@@ -4,4 +4,51 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
+        m = len(matrix)
+        if m == 0:
+            return []
+        n = len(matrix[0])
+        if n == 0:
+            return []
 
+        atlantic = [ [ False for j in range(n) ] for i in range(m) ]
+        pacific = [ [ False for j in range(n) ] for i in range(m) ]
+
+        atlanticBoundary = set((m - 1, j) for j in range(n)) | \
+                set((i, n - 1) for i in range(m))
+        for i, j in atlanticBoundary:
+            atlantic[i][j] = True
+        pacificBoundary = set((0, j) for j in range(n)) | \
+                set((i, 0) for i in range(m))
+        for i, j in pacificBoundary:
+            pacific[i][j] = True
+
+        def bfs(ocean, bd):
+            if not bd:
+                return
+            for i, j in list(bd):
+                if i > 0 and not ocean[i-1][j] and matrix[i-1][j] >= matrix[i][j]:
+                    ocean[i-1][j] = True
+                    bd.add((i-1, j))
+                if j > 0 and not ocean[i][j-1] and matrix[i][j-1] >= matrix[i][j]:
+                    ocean[i][j-1] = True
+                    bd.add((i, j-1))
+                if i < m - 1 and not ocean[i+1][j] and matrix[i+1][j] >= matrix[i][j]:
+                    ocean[i+1][j] = True
+                    bd.add((i+1, j))
+                if j < n - 1 and not ocean[i][j+1] and matrix[i][j+1] >= matrix[i][j]:
+                    ocean[i][j+1] = True
+                    bd.add((i, j+1))
+                bd.remove((i, j))
+
+            bfs(ocean, bd)
+
+        bfs(atlantic, atlanticBoundary)
+        bfs(pacific, pacificBoundary)
+
+        res = []
+        for i in range(m):
+            for j in range(n):
+                if atlantic[i][j] and pacific[i][j]:
+                    res.append([i, j])
+        return res

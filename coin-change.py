a741d8dab225020ef6b4ddccc6e652f6d62ba23f
Chun Yang <yang2007chun@gmail.com>
2017-02-10 23:05:36 -0600

    more

--- coin-change.py
+++ coin-change.py
@@ -1,21 +1,26 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        minAmounts = [0] + [-1] * amount
-        for i in range(1, amount + 1):
-            minAmount = -1
-            for c in coins:
-                if c <= i:
-                    preAmount = minAmounts[i - c]
-                    if preAmount >= 0:
-                        if minAmount > 0:
-                            minAmount = min(minAmount, preAmount + 1)
-                        else:
-                            minAmount = preAmount + 1
-            minAmounts[i] = minAmount
+def coinChange(coins, amount):
+    """
+    :type coins: List[int]
+    :type amount: int
+    :rtype: int
+    """
+    minAmounts = [-1] * (amount + 1)
+    minAmounts[0] = 0
 
-        return minAmounts[amount]
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if i >= coin and minAmounts[i - coin] >= 0:
+                if minAmounts[i] == -1:
+                    minAmounts[i] = minAmounts[i - coin] + 1
+                else:
+                    minAmounts[i] = min(minAmounts[i], minAmounts[i - coin] + 1)
+
+    return minAmounts[amount]
+
+# assert coinChange([], 100) == -1
+# assert coinChange([], 0) == 0
+# assert coinChange([2, 3], 5) == 2
+# assert coinChange([2, 3], 7) == 3
+# assert coinChange([2, 3, 6], 7) == 3
+# assert coinChange([2, 3, 7], 7) == 1
+# assert coinChange([2, 4, 6], 7) == -1

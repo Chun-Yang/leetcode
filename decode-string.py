505aaf6f944cda54aaa84e79e6f29d45192722aa
Chun Yang <yang2007chun@gmail.com>
2017-02-13 23:18:59 -0600

    more

--- decode-string.py
+++ decode-string.py
@@ -17,24 +17,24 @@
 #         repeats[-1][0] += c
 #     return repeats[0][0]
 
-# v2.0
-class Solution(object):
-    def decodeString(self, s):
-        numbers, strings = [], []
-        number, string = '', ''
-        for c in s:
-            if c == '[':
-                numbers.append(number)
-                strings.append(string)
-                number = ''
-                string = ''
-            elif c == ']':
-                string = strings.pop() + int(numbers.pop()) * string
-            elif c.isdigit():
-                number += c
-            else:
-                string += c
-        return string
+# # v2.0
+# class Solution(object):
+#     def decodeString(self, s):
+#         numbers, strings = [], []
+#         number, string = '', ''
+#         for c in s:
+#             if c == '[':
+#                 numbers.append(number)
+#                 strings.append(string)
+#                 number = ''
+#                 string = ''
+#             elif c == ']':
+#                 string = strings.pop() + int(numbers.pop()) * string
+#             elif c.isdigit():
+#                 number += c
+#             else:
+#                 string += c
+#         return string
 
 assert Solution().decodeString("") == ""
 assert Solution().decodeString("a") == "a"

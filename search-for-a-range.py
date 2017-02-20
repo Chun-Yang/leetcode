9602ba0d34557717ccd1afdf323e393bcc1bb8eb
Chun Yang <yang2007chun@gmail.com>
2017-02-19 22:19:01 -0600

    more

--- search-for-a-range.py
+++ search-for-a-range.py
@@ -1,34 +1,36 @@
+# 1. binary search * 2
+# class Solution(object):
+#     def searchRange(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         def search(t):
+#             lo, hi = 0, len(nums)
+#             while lo < hi:
+#                 mid = (lo + hi) // 2
+#                 if nums[mid] < t:
+#                     lo = mid + 1
+#                 else:
+#                     hi = mid
+#             return lo
+
+#         left = search(target)
+#         if left < len(nums) and nums[left] == target:
+#             return [left, search(target + 1) - 1]
+#         else:
+#             return [-1, -1]
+
+# 2. use lib
+from bisect import bisect_left, bisect_right
 class Solution(object):
     def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        if not nums:
+        left = bisect_left(nums, target)
+        if left < len(nums) and nums[left] == target:
+            return [left, bisect_right(nums, target) - 1]
+        else:
             return [-1, -1]
-
-        lo, hi = 0, len(nums) - 1
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if nums[mid] < target:
-                lo = mid + 1
-            else:
-                hi = mid
-        if nums[lo] != target:
-            return [-1, -1]
-
-        left = lo
-        hi = len(nums) - 1
-        while lo < hi:
-            mid = (lo + hi + 1) // 2
-            if nums[mid] > target:
-                hi = mid - 1
-            else:
-                lo = mid
-        right = hi
-        return [left, right]
-
 
 # assert Solution().searchRange([1,2,4,5,5], 3) == [-1, -1]
 # assert Solution().searchRange([1,2,3,3,5], 3) == [2, 3]

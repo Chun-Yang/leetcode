b334328607330fdb58284b4a7ae9bae74222c7c1
Chun Yang <yang2007chun@gmail.com>
2017-02-22 08:15:57 -0600

    more

--- combination-sum.py
+++ combination-sum.py
@@ -1,3 +1,33 @@
+# v1.0
+# class Solution(object):
+#     def combinationSum(self, candidates, target):
+#         """
+#         :type candidates: List[int]
+#         :type target: int
+#         :rtype: List[List[int]]
+#         """
+#         candidates = sorted(candidates, reverse=True)
+#         size = len(candidates)
+
+#         def dfs(startIndex, targetValue):
+#             if targetValue == 0:
+#                 return [[]]
+#             if startIndex == size:
+#                 return []
+
+#             combs = []
+#             startCount = 0
+#             startValue = candidates[startIndex]
+#             while targetValue >= startCount * startValue:
+#                 for comb in dfs(startIndex + 1, targetValue - startCount * startValue):
+#                     combs.append(comb + [startValue] * startCount)
+#                 startCount += 1
+
+#             return combs
+
+#         combs = dfs(0, target)
+#         return combs
+
 class Solution(object):
     def combinationSum(self, candidates, target):
         """
@@ -5,26 +35,15 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        candidates = sorted(candidates, reverse=True)
-        size = len(candidates)
+        combsForTargets = [ [] for i in range(target + 1) ]
+        combsForTargets[0] = [[]]
+        for i in range(1, target + 1):
+            for num in candidates:
+                if num <= i:
+                    for comb in combsForTargets[i - num]:
+                        if comb == [] or comb[-1] <= num:
+                            combsForTargets[i].append(comb + [num])
+        return combsForTargets[target]
 
-        def dfs(startIndex, targetValue):
-            if targetValue == 0:
-                return [[]]
-            if startIndex == size:
-                return []
-
-            combs = []
-            startCount = 0
-            startValue = candidates[startIndex]
-            while targetValue >= startCount * startValue:
-                for comb in dfs(startIndex + 1, targetValue - startCount * startValue):
-                    combs.append(comb + [startValue] * startCount)
-                startCount += 1
-
-            return combs
-
-        combs = dfs(0, target)
-        return combs
-
-# Solution().combinationSum([2,3,6,7], 7)
+# res = Solution().combinationSum([2,3,6,7], 7)
+# print("res", res)

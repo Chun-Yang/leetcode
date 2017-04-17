597471317d5f3c0abe2691e8b11d5f34a3a72f59
Chun Yang <yang2007chun@gmail.com>
2017-04-16 23:20:34 -0500

    mroe

---
+++ README.md
@@ -0,0 +1,7 @@
+- 69. Sqrt(x)
+  - for binary search, try to find the constrain for mid
+    as long as we get in the constrain, the mid will be correct
+  - for binary search, when we do ``` l + (r - l) / 2 ```,
+    we can not use (r - 1) to try to converge, since divide operation
+    goes to the left side: e.g. 1 / 2 == 1
+  - to avoid int overflow, we should use divide instead of multiply,
--- Solution.java
+++ Solution.java
@@ -1,52 +1,11 @@
-// import java.util.Arrays;
-// import java.util.ArrayList;
-// import java.util.List;
-// import java.util.HashSet;
-// import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class Solution {
-  public int mySqrt(int x) {
-    if (x == 0) return 0;
-
-    int left = 1;
-    int right = x;
-    int mid;
-
-    while (true) {
-      mid = left + (right - left) / 2;
-      if (mid > x / mid) {
-        right = mid - 1;
-      } else if (mid + 1 <= x / (mid + 1)) {
-        left = mid + 1;
-      } else {
-        return mid;
-      }
-    }
-
-    // while (left < right) {
-    //   mid = left + (right - left) / 2;
-    //   if (mid > x / mid) {
-    //     right = mid;
-    //   } else if (mid < x / mid) {
-    //     left = mid + 1;
-    //   } else {
-    //     return mid;
-    //   }
-    // }
-
-    // if (x / left < left) {
-    //   return left - 1;
-    // }
-    // return left;
+  public static void main (String[] args) {
+    Solution s = new Solution();
   }
-  // public static void main (String[] args) {
-  //   Solution s = new Solution();
-  //   assert s.mySqrt(0) == 0;
-  //   assert s.mySqrt(1) == 1;
-  //   assert s.mySqrt(2) == 1;
-  //   assert s.mySqrt(3) == 1;
-  //   assert s.mySqrt(4) == 2;
-  //   assert s.mySqrt(9) == 3;
-  //   assert s.mySqrt(2147395599) == 46339;
-  // }
 }

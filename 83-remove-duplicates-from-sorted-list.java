27e21c0b78c188fd1e13753210f4be417236a8c7
Chun Yang <yang2007chun@gmail.com>
2017-04-26 07:24:40 -0500

    more

---
+++ 83-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,16 @@
+public class Solution {
+  public ListNode deleteDuplicates(ListNode head) {
+    ListNode cur = head;
+    while (cur != null) {
+      if (cur.next != null && cur.val == cur.next.val) {
+        cur.next = cur.next.next;
+      } else {
+        cur = cur.next;
+      }
+    }
+    return head;
+  }
+  public static void main (String[] args) {
+    Solution s = new Solution();
+  }
+}
--- Solution.java
+++ Solution.java
@@ -1,15 +1,4 @@
 public class Solution {
-  public int searchInsert(int[] nums, int target) {
-    int lo = 0, mid = 0, hi = nums.length - 1;
-    while (lo <= hi) {
-      mid = lo + (hi - lo) / 2;
-      if (target < nums[mid]) hi = mid - 1;
-      else if (target > nums[mid]) lo = mid + 1;
-      else return mid;
-    }
-
-    return lo;
-  }
   public static void main (String[] args) {
     Solution s = new Solution();
   }

8ebdf733c40461ff1d659499f2e5b1921370b0b2
Chun-Yang <yang2007chun@gmail.com>
2017-08-30 10:23:20 -0500

    more

--- 332-reconstruct-itinerary.java
+++ 332-reconstruct-itinerary.java
@@ -20,60 +20,86 @@
 // Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"]. But
 // it is larger in lexical order.
 
-// dfs, pq for ordering
 import java.util.*;
 
+// https://discuss.leetcode.com/topic/36383/share-my-solution
+// Hierholzer's algorithm (greedy)
 class Solution {
-  public static void main (String[] args) {
-    Solution s = new Solution();
-    s.findItinerary(new String[][]{
-      new String[]{"JFK","KUL"},
-      new String[]{"JFK","NRT"},
-      new String[]{"NRT","JFK"},
-    });
-  }
+  Map<String, PriorityQueue<String>> flights;
+  LinkedList<String> path;
+
   public List<String> findItinerary(String[][] tickets) {
-    // build adjancency list
-    HashMap<String, List<String>> adjList = new HashMap<>();
+    flights = new HashMap<>();
+    path = new LinkedList<>();
     for (String[] ticket : tickets) {
-      if (adjList.containsKey(ticket[0])) adjList.get(ticket[0]).add(ticket[1]);
-      else adjList.put(ticket[0], new ArrayList<String>(Arrays.asList(ticket[1])));
+      flights.putIfAbsent(ticket[0], new PriorityQueue<>());
+      flights.get(ticket[0]).add(ticket[1]);
     }
-
-    // dfs to build itinerary until adjancency list is empty
-    List<String> itinerary = new ArrayList<String>(Arrays.asList("JFK"));
-    dfs(adjList, itinerary,  "JFK");
-    System.out.println(itinerary);
-    return itinerary;
+    dfs("JFK");
+    return path;
   }
-  // true: valid itinerary
-  private boolean dfs(HashMap<String, List<String>> adjList, List<String> itinerary, String from) {
-    if (adjList.isEmpty()) return true;
 
-    if (!adjList.containsKey(from)) return false;
-
-    List<String> destinations = adjList.get(from);
-
-    // back tracking
-    if (destinations.size() == 1) {
-      String destination = destinations.get(0);
-
-      adjList.remove(from);
-      itinerary.add(destination);
-      if (dfs(adjList, itinerary, destination)) return true;
-      itinerary.remove(itinerary.size() - 1);
-      adjList.put(from, destinations);
-    } else {
-      List<String> sortedDestinations = new ArrayList<>(destinations);
-      Collections.sort(sortedDestinations);
-      for (String destination : sortedDestinations) {
-        destinations.remove(destination);
-        itinerary.add(destination);
-        if (dfs(adjList, itinerary, destination)) return true;
-        itinerary.remove(itinerary.size() - 1);
-        destinations.add(destination);
-      }
+  public void dfs(String departure) {
+    PriorityQueue<String> arrivals = flights.get(departure);
+    if (arrivals != null && arrivals.size() > 0) {
+      dfs(arrivals.poll());
     }
-    return false;
+    path.addFirst(departure);
   }
 }
+
+// Solution 1 backtracking
+// dfs, pq for ordering
+// class Solution {
+//   public static void main (String[] args) {
+//     Solution s = new Solution();
+//     s.findItinerary(new String[][]{
+//       new String[]{"JFK","KUL"},
+//       new String[]{"JFK","NRT"},
+//       new String[]{"NRT","JFK"},
+//     });
+//   }
+//   public List<String> findItinerary(String[][] tickets) {
+//     // build adjancency list
+//     HashMap<String, List<String>> adjList = new HashMap<>();
+//     for (String[] ticket : tickets) {
+//       adjList.putIfAbsent(ticket[0], new ArrayList<String>());
+//       adjList.get(ticket[0]).add(ticket[1]);
+//     }
+
+//     // dfs to build itinerary until adjancency list is empty
+//     List<String> itinerary = new ArrayList<String>(Arrays.asList("JFK"));
+//     dfs(adjList, itinerary,  "JFK");
+//     return itinerary;
+//   }
+//   // true: valid itinerary
+//   private boolean dfs(HashMap<String, List<String>> adjList, List<String> itinerary, String from) {
+//     if (adjList.isEmpty()) return true;
+
+//     if (!adjList.containsKey(from)) return false;
+
+//     List<String> destinations = adjList.get(from);
+
+//     // back tracking
+//     if (destinations.size() == 1) {
+//       String destination = destinations.get(0);
+
+//       adjList.remove(from);
+//       itinerary.add(destination);
+//       if (dfs(adjList, itinerary, destination)) return true;
+//       itinerary.remove(itinerary.size() - 1);
+//       adjList.put(from, destinations);
+//     } else {
+//       List<String> sortedDestinations = new ArrayList<>(destinations);
+//       Collections.sort(sortedDestinations);
+//       for (String destination : sortedDestinations) {
+//         destinations.remove(destination);
+//         itinerary.add(destination);
+//         if (dfs(adjList, itinerary, destination)) return true;
+//         itinerary.remove(itinerary.size() - 1);
+//         destinations.add(destination);
+//       }
+//     }
+//     return false;
+//   }
+// }
--- Solution.java
+++ Solution.java
@@ -3,55 +3,5 @@ import java.util.*;
 public class Solution {
   public static void main (String[] args) {
     Solution s = new Solution();
-    s.findItinerary(new String[][]{
-      new String[]{"JFK","KUL"},
-      new String[]{"JFK","NRT"},
-      new String[]{"NRT","JFK"},
-    });
   }
-  public List<String> findItinerary(String[][] tickets) {
-    // build adjancency list
-    HashMap<String, List<String>> adjList = new HashMap<>();
-    for (String[] ticket : tickets) {
-      if (adjList.containsKey(ticket[0])) adjList.get(ticket[0]).add(ticket[1]);
-      else adjList.put(ticket[0], new ArrayList<String>(Arrays.asList(ticket[1])));
-    }
-
-    // dfs to build itinerary until adjancency list is empty
-    List<String> itinerary = new ArrayList<String>(Arrays.asList("JFK"));
-    dfs(adjList, itinerary,  "JFK");
-    System.out.println(itinerary);
-    return itinerary;
-  }
-  // true: valid itinerary
-  private boolean dfs(HashMap<String, List<String>> adjList, List<String> itinerary, String from) {
-    if (adjList.isEmpty()) return true;
-
-    if (!adjList.containsKey(from)) return false;
-
-    List<String> destinations = adjList.get(from);
-
-    // back tracking
-    if (destinations.size() == 1) {
-      String destination = destinations.get(0);
-
-      adjList.remove(from);
-      itinerary.add(destination);
-      if (dfs(adjList, itinerary, destination)) return true;
-      itinerary.remove(itinerary.size() - 1);
-      adjList.put(from, destinations);
-    } else {
-      List<String> sortedDestinations = new ArrayList<>(destinations);
-      Collections.sort(sortedDestinations);
-      for (String destination : sortedDestinations) {
-        destinations.remove(destination);
-        itinerary.add(destination);
-        if (dfs(adjList, itinerary, destination)) return true;
-        itinerary.remove(itinerary.size() - 1);
-        destinations.add(destination);
-      }
-    }
-    return false;
-  }
-
 }

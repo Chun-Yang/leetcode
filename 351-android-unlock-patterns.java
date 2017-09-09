c42d14cef0b91de2033c5ef4454a26a5e9a3e8da
Chun-Yang <yang2007chun@gmail.com>
2017-09-09 08:46:52 -0500

    more

--- 351-android-unlock-patterns.java
+++ 351-android-unlock-patterns.java
@@ -36,22 +36,61 @@
 // Example:
 // Given m = 1, n = 1, return 9.
 
+import java.util.*;
+
 class Solution {
+  public static void main (String[] args) {
+    Solution s = new Solution();
+    assert s.numberOfPatterns(1, 1) == 9;
+    assert s.numberOfPatterns(2, 2) == 56;
+    // assert s.numberOfPatterns(1, 2) == 65;
+  }
   public int numberOfPatterns(int m, int n) {
     int CELL_COUNT = 9;
     int count = 0;
-    // TODO: create cross
-    int[][] cross = new int[][]{
+    // create cross
+    int[][] cross = new int[CELL_COUNT][CELL_COUNT];
+    for (int i=0; i<CELL_COUNT; i++) Arrays.fill(cross[i], -1);
+    cross[0][2] = 1;
+    cross[0][6] = 3;
+    cross[0][8] = 4;
+    cross[2][0] = 1;
+    cross[2][6] = 4;
+    cross[2][8] = 5;
+    cross[6][0] = 3;
+    cross[6][2] = 4;
+    cross[6][8] = 7;
+    cross[8][0] = 4;
+    cross[8][2] = 5;
+    cross[8][6] = 7;
+    cross[1][7] = 4;
+    cross[3][5] = 4;
+    cross[5][3] = 4;
+    cross[7][1] = 4;
 
-    };
-    int[] visited = new int[CELL_COUNT];
+    boolean[] visited = new boolean[CELL_COUNT];
     for (int i=m; i<=n; i++) {
-      for (int start=0; start<CELL_COUNT; i++) {
-        // TODO: implment dfs
-        count += dfs(i, start, visited, cross);
+      for (int start=0; start<CELL_COUNT; start++) {
+        // back tracking
+        visited[start] = true;
+        count += dfs(i-1, start, visited, cross);
+        visited[start] = false;
       }
     }
     return count;
   }
 
+  int dfs(int movesLeft, int lastMove, boolean[] visited, int[][] cross) {
+    if (movesLeft == 0) return 1;
+
+    int count = 0;
+    for (int i=0; i<9; i++) {
+      if (!visited[i] && (cross[lastMove][i] == -1 || visited[cross[lastMove][i]])) {
+        visited[i] = true;
+        count += dfs(movesLeft - 1, i, visited, cross);
+        visited[i] = false;
+      }
+    }
+    return count;
+  }
 }

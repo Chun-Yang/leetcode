b0ef92710b7096e4d01807bc8f6111ca631f92af
Chun-Yang <yang2007chun@gmail.com>
2017-09-18 09:43:20 -0500

    more

---
+++ 372-super-pow.java
@@ -0,0 +1,11 @@
+// Your task is to calculate a^b mod 1337 where
+// a is a positive integer and
+// b is an extremely large positive integer given in the form of an array.
+
+// Solution: repeat pattern
+// https://discuss.leetcode.com/topic/50504/java-4ms-solution-using-the-remainder-repeat-pattern/
+class Solution {
+  public int superPow(int a, int[] b) {
+
+  }
+}

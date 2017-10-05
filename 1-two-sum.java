2c871e1db84c425516dadd1270f028ae15786d04
Chun-Yang <yang2007chun@gmail.com>
2017-10-04 23:21:43 -0500

    more

--- 1-two-sum.java
+++ 1-two-sum.java
@@ -1,10 +1,21 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//
+// Example:
+// Given nums = [2, 7, 11, 15], target = 9,
+//
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.HashMap;
 
-public class Solution {
+class Solution {
   public int[] twoSum(int[] nums, int target) {
     HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
     for (int i=0; i < nums.length; i++) {
--- scriper.js
+++ scriper.js
@@ -4,18 +4,33 @@ const scrapeIt = require("scrape-it");
 const questionsString = require('./scriper-string')
 
 const questions = questionsString.split('\n').map((s) => {
-  const [index, href, title] = s.split(',')
+  const [index, href, title] = s.split(',').map((s) => s.toLowerCase())
   return { index, href, title }
 })
 
-function stringify (index, title) {
-  return [index, ...title.split(' ')].join('-') + '.java'
+function stringify (index, href) {
+  return [index, href.split('/')[2]].join('-') + '.java'
 }
 
-questions.map(async (props) => {
+questions.forEach((props) => {
   const { index, href, title } = props
-  return scrapeIt(href, { q: '.question-description' })
+  const fullHref = 'https://leetcode.com' + href
+  const valid = index > 2 && index < 50
+  if (!valid) {
+    return
+  }
+  scrapeIt(fullHref, { q: '.question-description' })
     .then(({ q }) => {
-      fs.writeSync('../pages/${stringify(index, title)}', q)
+      const fileName = stringify(index, href)
+      const formattedQ = q.split('\n').map(s => '// ' + s.trim()).join('\n')
+      fs.writeFile(fileName, formattedQ, (e) => {
+        if (e) {
+          console.log('FILE ERROR: ', e)
+        }
+      })
+      // fs.writeSync('./pages/${stringify(index, title)}', q)
+    })
+    .catch((e) => {
+      console.log('PROMISE ERROR: ', e)
     })
 })

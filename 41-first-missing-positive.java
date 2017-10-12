22ed67b7651236b1228ea2af4ea0123697245aa1
Chun-Yang <yang2007chun@gmail.com>
2017-10-12 12:14:19 -0500

    more

--- 41-first-missing-positive.java
+++ 41-first-missing-positive.java
@@ -1,14 +1,48 @@
 // Given an unsorted integer array, find the first missing positive integer.
-// 
-// 
-// 
+//
+//
+//
 // For example,
-// Given [1,2,0] return 3,
-// and [3,4,-1,1] return 2.
-// 
-// 
-// 
+// [1,2,0] 3
+// [3,4,-1,1] 2
+// [2,2,3] 1
+//
+//
+//
 // Your algorithm should run in O(n) time and uses constant space.
+// NOTE: first means
 
 
 // https://leetcode.com/problems/first-missing-positive
+
+// Solution1:
+// two pass:
+// pointer jump + change positive to negative
+// select the first index whose value is positive
+// [1,2,0] [-1,-2,0]
+
+// after processing
+// >= 0 means not visited
+// -1 means visited
+class Solution {
+  public int firstMissingPositive(int[] nums) {
+    for (int i=0; i<nums.length; i++) {
+      if (nums[i] <= 0) nums[i] = 0;
+    }
+    for (int i=0; i<nums.length; i++) {
+      int index = nums[i] - 1;
+      while(index >= 0 && index < nums.length) {
+        int nextIndex = nums[index] - 1;
+        nums[index] = -1;
+        index = nextIndex;
+      }
+    }
+    for (int i=0; i<nums.length; i++) {
+      if (nums[i] >= 0) return i + 1;
+    }
+    return nums.length + 1;
+  }
+}
+
+// Solution2: move value to its own position
+// https://discuss.leetcode.com/topic/1361/a-very-nice-solution-from-ants-aasma-stackoverflow

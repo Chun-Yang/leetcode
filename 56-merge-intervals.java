d9c611adf1d0a22202e5f789225286d528e79cac
Chun-Yang <yang2007chun@gmail.com>
2017-10-21 14:51:41 -0500

    more

--- 56-merge-intervals.java
+++ 56-merge-intervals.java
@@ -1,6 +1,12 @@
+// Given a collection of intervals, merge all overlapping intervals.
+
+// For example,
+// Given [1,3],[2,6],[8,10],[15,18],
+// return [1,6],[8,10],[15,18].
+
 import java.util.*;
 
-public class Solution {
+class Solution {
   public static void main (String[] args) {
     Solution s = new Solution();
   }
--- 58-length-of-last-word.java
+++ 58-length-of-last-word.java
@@ -1,13 +1,28 @@
-// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
-// 
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+// return the length of last word in the string.
+//
 // If the last word does not exist, return 0.
-// 
+//
 // Note: A word is defined as a character sequence consists of non-space characters only.
-// 
-// 
+//
+//
 // For example,
 // Given s = "Hello World",
 // return 5.
 
 
 // https://leetcode.com/problems/length-of-last-word
+
+// Solution1: pointer (start of last word)
+
+class Solution {
+  public int lengthOfLastWord(String s) {
+    // end is exclusive
+    int end = s.length();
+    while (end > 0 && s.charAt(end-1) == ' ') end--; // skip all spaces
+    // start is inclusive
+    int start = end;
+    while (start > 0 && s.charAt(start-1) != ' ') start--;
+    return end - start;
+  }
+}

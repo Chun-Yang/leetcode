32cb93f431778a019998504fec10ec6e17a926cf
Chun-Yang <yang2007chun@gmail.com>
2017-10-21 15:41:01 -0500

    more

--- 59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii.java
@@ -1,11 +1,11 @@
 // Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
-// 
-// 
+//
+//
 // For example,
 // Given n = 3,
-// 
+//
 // You should return the following matrix:
-// 
+//
 // [
 // [ 1, 2, 3 ],
 // [ 8, 9, 4 ],
@@ -14,3 +14,26 @@
 
 
 // https://leetcode.com/problems/spiral-matrix-ii
+
+class Solution {
+  public int[][] generateMatrix(int n) {
+    int[][] matrix = new int[n][n];
+
+    int start = 0;
+    int end = n - 1;
+
+    int value = 1;
+    while (start < end) {
+      for (int j=start; j<end; j++) matrix[start][j] = value++;
+      for (int i=start; i<end; i++) matrix[i][end] = value++;
+      for (int j=end; j>start; j--) matrix[end][j] = value++;
+      for (int i=end; i>start; i--) matrix[i][start] = value++;
+      start++;
+      end--;
+    }
+
+    if (start == end) matrix[start][end] = value;
+
+    return matrix;
+  }
+}

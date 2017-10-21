86b0daac02baea37ca1ba95f9f1c713e5d6017c1
Chun-Yang <yang2007chun@gmail.com>
2017-10-20 23:23:49 -0500

    more

--- 49-group-anagrams.java
+++ 49-group-anagrams.java
@@ -1,6 +1,6 @@
 // Given an array of strings, group anagrams together.
-// 
-// 
+//
+//
 // For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
 // Return:
 // [
@@ -8,8 +8,19 @@
 // ["nat","tan"],
 // ["bat"]
 // ]
-// 
+//
 // Note: All inputs will be in lower-case.
 
 
 // https://leetcode.com/problems/group-anagrams
+
+// Solution1:
+// HashMap key is the frequency array in string
+// value is an ArrayList that contains strings
+
+// Solution2:
+// similar to solution1, but use sorted chars as key
+
+// Solution3:
+// similar to solution1, but use multiplication of prime number as key
+// https://discuss.leetcode.com/topic/45639/java-beat-100-use-prime-number
---
+++ 50-powx-n.java
@@ -0,0 +1,54 @@
+// Implement pow(x, n).
+
+// Solution1: binary search, rich to the top, then reduce (iterative)
+// gredually (NOTE: this is not binary search the following is a good implement)
+// https://discuss.leetcode.com/topic/40546/iterative-log-n-solution-with-clear-explanation
+
+// Solution2: recursive using odd and even (recursive)
+// https://discuss.leetcode.com/topic/5425/short-and-easy-to-understand-solution
+
+// Solution3: several approachs
+// https://discuss.leetcode.com/topic/21837/5-different-choices-when-talk-with-interviewers
+
+// recursive preorder
+// class Solution {
+//   public double myPow(double x, int n) {
+//     if (n == Integer.MIN_VALUE) return 1 / x * myPow(x, n + 1);
+//     if (n < 0) return myPow(1/x, -n);
+//     if (n == 0) return 1;
+//     double half = myPow(x, n / 2);
+//     if (n % 2 == 0) return half * half;
+//     return x * half * half;
+//   }
+// }
+
+// recursive postorder (tail recursion)
+// class Solution {
+//   public double myPow(double x, int n) {
+//     if (n == Integer.MIN_VALUE) return 1 / x * myPow(x, n + 1);
+//     if (n < 0) return myPow(1/x, -n);
+//     if (n == 0) return 1;
+//     if (n % 2 == 0) return myPow(x*x, n / 2);
+//     else return x * myPow(x*x, n / 2);
+//   }
+// }
+
+// iterative
+// y = x ^ (a1 + 2 * a2 + 4 * a3 ...)
+// y = x ^ (a1) * x ^ (2 * a2) * ...
+// where ai is 0 or 1
+class Solution {
+  public double myPow(double x, int n) {
+    if (n == Integer.MIN_VALUE) return 1 / x * myPow(x, n + 1);
+    if (n < 0) return myPow(1/x, -n);
+
+    double pow = 1;
+    double base = x;
+    while (n > 0) {
+      if (n % 2 == 1) pow *= base;
+      base *= base;
+      n /= 2;
+    }
+    return pow;
+  }
+}

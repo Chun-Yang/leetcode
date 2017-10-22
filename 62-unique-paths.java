062acb01a366c55906f8bdb75e46842de1eff4fe
Chun-Yang <yang2007chun@gmail.com>
2017-10-22 11:48:56 -0500

    more

--- 62-unique-paths.java
+++ 62-unique-paths.java
@@ -1,15 +1,18 @@
 // A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
-// 
+//
 // The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
-// 
+//
 // How many possible unique paths are there?
-// 
-// 
-// 
+//
+//
+//
 // Above is a 3 x 7 grid. How many possible unique paths are there?
-// 
-// 
+//
+//
 // Note: m and n will be at most 100.
 
 
 // https://leetcode.com/problems/unique-paths
+
+// Solution1: C(m-1 + n-1) (m-1)
+// time: O(Math.max(m, n))
--- 63-unique-paths-ii.java
+++ 63-unique-paths-ii.java
@@ -1,21 +1,45 @@
 // Follow up for "Unique Paths":
-// 
+//
 // Now consider if some obstacles are added to the grids. How many unique paths would there be?
-// 
+//
 // An obstacle and empty space is marked as 1 and 0 respectively in the grid.
-// 
+//
 // For example,
 // There is one obstacle in the middle of a 3x3 grid as illustrated below.
-// 
+//
 // [
 // [0,0,0],
 // [0,1,0],
 // [0,0,0]
 // ]
-// 
+//
 // The total number of unique paths is 2.
-// 
+//
 // Note: m and n will be at most 100.
 
 
 // https://leetcode.com/problems/unique-paths-ii
+
+// Solution1: dp
+// time: O(m*n) space: O(m*n)
+// NOTE: we can improve the space complexity to O(Math.min(m,n))
+class Solution {
+  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int m = obstacleGrid.length;
+    int n = obstacleGrid[0].length;
+    int[][] paths = new int[m][n];
+    paths[0][0] = (1-obstacleGrid[0][0]);
+    for (int i=1; i<m; i++) {
+      paths[i][0] = paths[i-1][0] * (1 - obstacleGrid[i][0]);
+    }
+    for (int j=1; j<n; j++) {
+      paths[0][j] = paths[0][j-1] * (1 - obstacleGrid[0][j]);
+    }
+    for (int i=1; i<m; i++) {
+      for (int j=1; j<n; j++) {
+        paths[i][j] = (1 - obstacleGrid[i][j]) * (paths[i-1][j] + paths[i][j-1]);
+      }
+    }
+    return paths[m-1][n-1];
+  }
+}

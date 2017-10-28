3587ef0c5d5211b6b79e00fb4bfa94f830811fe7
Chun-Yang <yang2007chun@gmail.com>
2017-10-27 23:38:52 -0500

    more

--- 117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii.java
@@ -28,5 +28,63 @@
 
 // https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii
 
-// dfs
+// solution1: dfs
 // passdown the parent, find the next one
+// class Solution {
+//   public void connect(TreeLinkNode root) {
+//     dfs(new TreeLinkNode(0), root, false);
+//   }
+//   private void dfs(TreeLinkNode parent, TreeLinkNode current, boolean isLeft) {
+//     if (current == null) {
+//       return;
+//     }
+//     if (isLeft && parent.right != null) {
+//       current.next = parent.right;
+//     } else {
+//       parent = parent.next;
+//       while(parent != null) {
+//         if (parent.left != null) {
+//           current.next = parent.left;
+//           break;
+//         } else if (parent.right != null) {
+//           current.next = parent.right;
+//           break;
+//         }
+//         parent = parent.next;
+//       }
+//     }
+//     dfs(current, current.right, false);
+//     dfs(current, current.left, true);
+//   }
+// }
+
+// solution2: bfs and linked list pointer
+class Solution {
+  public void connect(TreeLinkNode root) {
+    TreeLinkNode parent = root;
+    while (parent != null) {
+      TreeLinkNode child = null;
+      TreeLinkNode nextParent = null;
+      while(parent != null) {
+        if (parent.left != null) {
+          if (child == null) {
+            nextParent = parent.left;
+          } else {
+            child.next = parent.left;
+          }
+          child = parent.left;
+        }
+        if (parent.right != null) {
+          if (child == null) {
+            nextParent = parent.right;
+          } else {
+            child.next = parent.right;
+          }
+          child = parent.right;
+        }
+        parent = parent.next;
+      }
+      parent = nextParent;
+    }
+  }
+}

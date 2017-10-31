592d163d3516b523634cc7a8522f991a5f8004c8
Chun-Yang <yang2007chun@gmail.com>
2017-10-31 15:40:58 -0500

    change

--- 134-gas-station.java
+++ 134-gas-station.java
@@ -23,3 +23,35 @@
 // solution1 two pass
 // - find the remaining one that is positive till the end
 // - use that index to see if we can go back to its self
+
+class Solution {
+  // invariant: total >= 0
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    int n = gas.length;
+    int index = 0;
+    int total = 0;
+    for (int i=0; i<n; i++) {
+      total += gas[i] - cost[i];
+      if (total < 0) {
+        index = i + 1;
+        total = 0;
+      }
+    }
+    if (index == n) {
+      return -1;
+    }
+    total = 0;
+    for (int i=0; i<n; i++) {
+      int j = (i + index) % n;
+      total += gas[j] - cost[j];
+      if (total < 0) {
+        return -1;
+      }
+    }
+    return index;
+  }
+}
+
+// solution2 one pass
+// - save the total, if total is not negative, then it's guaranteed to
+// make a circle
--- 135-candy.java
+++ 135-candy.java
@@ -1,14 +1,41 @@
 // There are N children standing in a line. Each child is assigned a rating value.
-// 
-// 
+//
+//
 // You are giving candies to these children subjected to the following requirements:
-// 
-// 
+//
+//
 // Each child must have at least one candy.
 // Children with a higher rating get more candies than their neighbors.
-// 
-// 
+//
+//
 // What is the minimum candies you must give?
 
 
 // https://leetcode.com/problems/candy
+
+// two pass, left -> right, right -> left
+
+import java.util.*;
+
+class Solution {
+  public int candy(int[] ratings) {
+    int n = ratings.length;
+    int[] candies = new int[n];
+    Arrays.fill(candies, 1);
+    for (int i=1; i<n; i++) {
+      if (ratings[i] > ratings[i-1]) {
+        candies[i] = candies[i-1] + 1;
+      }
+    }
+    for (int i=n-2; i>=0; i--) {
+      if (ratings[i] > ratings[i+1]) {
+        candies[i] = Math.max(candies[i], candies[i+1] + 1);
+      }
+    }
+    int total = 0;
+    for (int i=0; i<n; i++) {
+      total += candies[i];
+    }
+    return total;
+  }
+}

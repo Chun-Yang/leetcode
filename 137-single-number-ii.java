bb0a102227e2b4b6ee8790fbd375f6c5b734282d
Chun-Yang <yang2007chun@gmail.com>
2017-11-01 09:07:41 -0500

    more

--- 137-single-number-ii.java
+++ 137-single-number-ii.java
@@ -1,9 +1,18 @@
-// Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
-// 
-// 
-// 
+// Given an array of integers, every element appears three times except for one,
+// which appears exactly once. Find that single one.
+//
+//
+//
 // Note:
-// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+// Your algorithm should have a linear runtime complexity.
+// Could you implement it without using extra memory?
 
 
 // https://leetcode.com/problems/single-number-ii
+// 0 -> 0, 0
+// 1 -> 0, 1
+// 2 -> 1, 1
+// 3 -> 0, 0
+// second = (first & num) ^ second
+// first = (first | num) ^ preSecond
+// probably need a true/fasle table

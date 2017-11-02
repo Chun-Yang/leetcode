f8e79a235976c748dda2f3121d101c28c1f832b5
Chun-Yang <yang2007chun@gmail.com>
2017-11-02 09:33:28 -0500

    change

--- 141-linked-list-cycle.java
+++ 141-linked-list-cycle.java
@@ -1,9 +1,20 @@
 // Given a linked list, determine if it has a cycle in it.
-// 
-// 
-// 
+//
+//
+//
 // Follow up:
 // Can you solve it without using extra space?
 
 
 // https://leetcode.com/problems/linked-list-cycle
+
+// solution1: slow and fast pointers
+// -----
+//     /\_meeting point
+//     \/
+// a = before circle begin
+// b = circle begin to meeting point
+// c = meeting point to circle begin
+// b + c is the circle length
+// a + b + (b + c) * n = 2 * ( a + b ) where n >= 1
+// (b + c) * (n - 1) + c = a

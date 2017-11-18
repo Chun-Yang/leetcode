7e6b57847cdb00af470d8319c48b243cd5ba0353
Chun-Yang <yang2007chun@gmail.com>
2017-11-18 15:34:11 -0600

    change

--- 222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes.java
@@ -1,7 +1,76 @@
 // Given a complete binary tree, count the number of nodes.
-// 
+//
 // Definition of a complete binary tree from Wikipedia:
-// In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+// In a complete binary tree every level, except possibly the last, is
+// completely filled, and all nodes in the last level are as far left as
+// possible. It can have between 1 and 2h nodes inclusive at the last level h.
 
 
 // https://leetcode.com/problems/count-complete-tree-nodes
+
+// solution1: count depth, use each node to determin the depth
+// time: O(log(n) * log(n))
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// class Solution {
+//   public int countNodes(TreeNode root) {
+//     int leftDepth = getLeftDepth(root);
+//     int rightDepth = getRightDepth(root);
+//     if (leftDepth == rightDepth) {
+//       return (1 << leftDepth) - 1;
+//     }
+//     return 1 + countNodes(root.left) + countNodes(root.right);
+//   }
+//   private int getLeftDepth(TreeNode root) {
+//     int depth = 0;
+//     while (root != null) {
+//       depth++;
+//       root = root.left;
+//     }
+//     return depth;
+//   }
+//   private int getRightDepth(TreeNode root) {
+//     int depth = 0;
+//     while (root != null) {
+//       depth++;
+//       root = root.right;
+//     }
+//     return depth;
+//   }
+// }
+
+// solution2: get height of left node and right node
+// same:     left is full,         right may be not
+// not same: left may not be full, right is
+// https://discuss.leetcode.com/topic/15533/concise-java-solutions-o-log-n-2
+class Solution {
+  int height(TreeNode root) {
+    int depth = 0;
+    while (root != null) {
+      root = root.left;
+      depth++;
+    }
+    return depth;
+  }
+  public int countNodes(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int currentHeight = height(root);
+    int rightHeight = height(root.right);
+    if (currentHeight == rightHeight + 1) {
+      return (1 << rightHeight) + countNodes(root.right);
+    } else {
+      return (1 << rightHeight) + countNodes(root.left);
+    }
+  }
+}
--- 224-basic-calculator.java
+++ 224-basic-calculator.java
@@ -1,19 +1,27 @@
 // Implement a basic calculator to evaluate a simple expression string.
-// 
-// The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces  .
-// 
+//
+// The expression string may contain open ( and closing parentheses ),
+// the plus + or minus sign -, non-negative integers and empty spaces  .
+//
 // You may assume that the given expression is always valid.
-// 
+//
 // Some examples:
-// 
+//
 // "1 + 1" = 2
 // " 2-1 + 2 " = 3
 // "(1+(4+5+2)-3)+(6+8)" = 23
-// 
-// 
-// 
-// 
+//
+//
+//
+//
 // Note: Do not use the eval built-in library function.
 
 
 // https://leetcode.com/problems/basic-calculator
+
+// solution1: stack
+// use a stack to track +/- signs before a bracket
+// signMultiplier = 1 change when we have a sign, if we have a bracket, we times the current sign
+// lastSign = '+' change when we have a sign
+// when we encouter a (, we push lastSign in the stack
+// toggle signMultiplier when we encouter a ) by poping stack
